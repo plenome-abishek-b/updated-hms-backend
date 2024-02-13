013c79a8cae2b59c717ee9733402bd592df16ac9
plenome-abishek-b <abhishek.baskar@plenome.com>
2024-02-13 15:22:23 +0530

    updated disabled patient API

--- src/setup-patient-disabled_patient_list/setup-patient-disabled_patient_list.module.ts
+++ src/setup-patient-disabled_patient_list/setup-patient-disabled_patient_list.module.ts
@@ -1,7 +1,7 @@
 import { Module } from '@nestjs/common';
 import { SetupPatientDisabledPatientListService } from './setup-patient-disabled_patient_list.service';
 import { SetupPatientDisabledPatientListController } from './setup-patient-disabled_patient_list.controller';
-
+ 
 @Module({
   controllers: [SetupPatientDisabledPatientListController],
   providers: [SetupPatientDisabledPatientListService],
